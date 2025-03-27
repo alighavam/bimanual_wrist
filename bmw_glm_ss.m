ae2f2c4357360f35bccc0c1cdb3503dd2eb66c33
Ali Ghavampour <53136447+alighavam@users.noreply.github.com>
2025-03-27 17:16:41 -0400

    testing glm fit

--- bmw_glm_ss.m
+++ bmw_glm_ss.m
@@ -173,6 +173,74 @@ function varargout = bmw_glm_ss(what, varargin)
             events.duration = events.duration ./ 1000;
             
             varargout{1} = events;
+        
+        case 'GLM:make_glm3'
+            % run with hrf_params = [7 16 1 1 1 0 32]
+            dat_file = dir(fullfile(baseDir, behavDir, participant_id, 'BimanualWrist_MR_*.dat'));
+            D = dload(fullfile(dat_file.folder, dat_file.name));
+            D = getrow(D, ismember(D.BN,runs));
+            
+            angles = [0,60,120,180,240,300];
+            
+            events.BN = [];
+            events.TN = [];
+            events.onset = [];
+            events.duration = [];
+            events.eventtype = [];
+            events.Uni_or_Bi = [];
+            events.hand = []; % 0: left, 1: right
+            events.angle_left = []; % 0, 60, 120, 180, 240, 300
+            events.angle_right = []; % 0, 60, 120, 180, 240, 300
+            
+            % LEFT HAND:
+            for i = 1:length(angles)
+                rows = D.Uni_or_Bi==0  & D.Hand==0 & D.targetAngle_L==angles(i);
+                events.BN = [events.BN; D.BN(rows)];
+                events.TN = [events.TN; D.TN(rows)];
+                events.onset = [events.onset; D.startTimeReal(rows)+D.time2plan(rows)];
+                events.duration = [events.duration; repmat(10, [sum(rows), 1])];
+                events.eventtype = [events.eventtype; repmat({sprintf('lhand:%d', angles(i))}, [sum(rows), 1])];
+                events.Uni_or_Bi = [events.Uni_or_Bi; D.Uni_or_Bi(rows)];
+                events.hand = [events.hand; D.Hand(rows)];
+                events.angle_left = [events.angle_left; repmat(angles(i), [sum(rows), 1])];
+                events.angle_right = [events.angle_right; repmat(-1, [sum(rows), 1])];
+            end
+            
+            % RIGHT HAND:
+            for i = 1:length(angles)
+                rows = D.Uni_or_Bi==0  & D.Hand==1 & D.targetAngle_R==angles(i);
+                events.BN = [events.BN; D.BN(rows)];
+                events.TN = [events.TN; D.TN(rows)];
+                events.onset = [events.onset; D.startTimeReal(rows)];
+                events.duration = [events.duration; repmat(10, [sum(rows), 1])];
+                events.eventtype = [events.eventtype; repmat({sprintf('rhand:%d', angles(i))}, [sum(rows), 1])];
+                events.Uni_or_Bi = [events.Uni_or_Bi; D.Uni_or_Bi(rows)];
+                events.hand = [events.hand; D.Hand(rows)];
+                events.angle_left = [events.angle_left; repmat(-1, [sum(rows), 1])];
+                events.angle_right = [events.angle_right; repmat(angles(i), [sum(rows), 1])];
+            end
+
+            % BIMANUAL:
+            for i = 1:length(angles)
+                for j = 1:length(angles)
+                    rows = D.Uni_or_Bi==1 & D.targetAngle_L==angles(i) & D.targetAngle_R==angles(j);
+                    events.BN = [events.BN; D.BN(rows)];
+                    events.TN = [events.TN; D.TN(rows)];
+                    events.onset = [events.onset; D.startTimeReal(rows)];
+                    events.duration = [events.duration; repmat(10, [sum(rows), 1])];
+                    events.eventtype = [events.eventtype; repmat({sprintf('bi:%d_%d',angles(i),angles(j))}, [sum(rows), 1])];
+                    events.Uni_or_Bi = [events.Uni_or_Bi; D.Uni_or_Bi(rows)];
+                    events.hand = [events.hand; repmat(2, [sum(rows), 1])];
+                    events.angle_left = [events.angle_left; repmat(angles(i), [sum(rows), 1])];
+                    events.angle_right = [events.angle_right; repmat(angles(j), [sum(rows), 1])];
+                end
+            end
+            
+            events = struct2table(events);
+            events.onset = events.onset ./ 1000;
+            events.duration = events.duration ./ 1000;
+            
+            varargout{1} = events;
 
         case 'GLM:make_event'
             operation  = sprintf('GLM:make_glm%d', glm);
--- test_time_series.m
+++ test_time_series.m
@@ -1,4 +1,4 @@
-sn = 6;
+sn = 4;
 T = load(['/Users/alighavampour/Desktop/Projects/bimanual_wrist/data/fMRI/ROI/s' num2str(sn,'%.2d') '/time_series_glm1.mat']);
 spm_file = load(['/Users/alighavampour/Desktop/Projects/bimanual_wrist/data/fMRI/glm1/s' num2str(sn,'%.2d') '/SPM.mat']);
 SPM = spm_file.SPM;
@@ -22,3 +22,107 @@ for run = runs
     plot(x, y_hat, 'r', 'linewidth', 1.5);
     title('run %d',run)
 end
+
+%% Fit hrf params - redo GLM:
+sn = 4;
+glm = 2;
+
+T = load(['/Users/alighavampour/Desktop/Projects/bimanual_wrist/data/fMRI/ROI/s' num2str(sn,'%.2d') '/time_series_glm' num2str(glm) '.mat']);
+spm_file = load(['/Users/alighavampour/Desktop/Projects/bimanual_wrist/data/fMRI/glm' num2str(glm) '/s' num2str(sn,'%.2d') '/SPM.mat']);
+SPM = spm_file.SPM;
+% load ROI definition (R)
+% R = load(fullfile(baseDir, regDir, participant_id, sprintf('s%s_%s_glm%d_region.mat', num2str(sn,'%.2d'), atlas, glm))); R=R.R;
+R = load(['/Users/alighavampour/Desktop/Projects/bimanual_wrist/data/fMRI/ROI/s' num2str(sn,'%.2d') sprintf('/s%s_ROI_glm%d_region.mat', num2str(sn,'%.2d'), glm)]);
+R=R.R;
+
+region_data = region_getdata(SPM.xY.VY,R);
+
+%%
+
+r = 2;
+hrf_params = [6 16 1 1 6 0 32];
+pre = 8;
+post = 22;
+run = 1;
+
+Yraw = region_data{r}; % Pick out data as a T x P matrix 
+
+% Get the hemodynamic response in micro-time resolution
+SPM.xBF.UNITS    = 'secs'; % units of the hrf
+SPM.xBF.T        = 16; % microtime resolution: number of time samples per scan
+SPM.xBF.dt       = SPM.xY.RT/SPM.xBF.T;  % Delta-t per sample 
+SPM.xBF.T0       = 1; % first time bin
+SPM.xBF.name     = 'fitted_hrf';
+SPM.xBF.order    = 1;
+SPM.xBF.Volterra = 1;  % volterra expansion order?
+SPM.xBF.bf = spm_hrf(SPM.xBF.dt, hrf_params);
+SPM.xBF.length = size(SPM.xBF.bf,1)*SPM.xBF.dt; % support in seconds 
+
+% Reconvolve the design matrix with new HRF
+SPM = spmj_glm_convolve(SPM);
+
+% Restimate the betas and get predicted and residual response
+[beta, Yhat, Yres] = spmj_glm_fit(SPM,Yraw);
+
+% Diagnostic plots
+figure;
+t=[SPM.xBF.dt*SPM.xBF.T0:SPM.xBF.dt:SPM.xBF.length]; 
+plot(t,SPM.xBF.bf);
+drawline([0],'dir','horz','linestyle','-');
+title(sprintf('params = %s',num2str(hrf_params)));
+
+% Run, convolved design matrix, sum of regressors of interest
+figure;
+X=SPM.xX.X(SPM.Sess(run).row,SPM.Sess(run).col);
+plot(sum(X,2));
+title(sprintf('Run %d - overall response', run));
+
+figure;
+Yhat_run1 = mean(Yhat(SPM.Sess(run).row,:),2);
+Yres_run1 = mean(Yres(SPM.Sess(run).row,:),2);
+Yadj_run1 = Yres_run1+Yres_run1; 
+t= SPM.Sess(run).row;
+hold on;
+plot(t, Yhat_run1, 'r', 'LineWidth', 1.5)
+plot(t, Yadj_run1, 'k', 'LineWidth', 1)
+title(sprintf('Run %d - overall response', run));
+
+% Get onset structure, cut-out the trials of choice, and plot evoked
+% response
+figure;
+D = spmj_get_ons_struct(SPM);
+Yadj = Yres+Yhat; 
+for i=1:size(D.block,1)
+    D.y_adj(i,:)=cut(mean(Yadj,2),pre,round(D.ons(i))-1,post,'padding','nan')';
+    D.y_hat(i,:)=cut(mean(Yhat,2),pre,round(D.ons(i))-1,post,'padding','nan')';
+    D.y_res(i,:)=cut(mean(Yres,2),pre,round(D.ons(i))-1,post,'padding','nan')';
+end
+                
+T = getrow(D,mod(D.num,2)==1); % Get the first onset for each double 
+traceplot([-pre:post],T.y_adj,'errorfcn','stderr'); % ,
+hold on;
+traceplot([-pre:post],T.y_hat,'linestyle','--',...
+        'linewidth',3); %
+drawline([-7,0,7,14,21],'dir','vert','linestyle',':');
+
+drawline([0],'dir','vert','linestyle','--');
+drawline([0],'dir','horz','linestyle','-');
+hold off;
+xlabel('TR');
+ylabel('activation');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
